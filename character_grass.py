b78b60ac0e594127e0db1aca5e0afccd7f448562
jjuncode <jjun.code@gmail.com>
2023-09-18 12:21:58 +0900

    중간커밋

--- character_grass.py
+++ character_grass.py
@@ -1,18 +1,51 @@
 from pico2d import *
-
-open_canvas()
+import math
 
 open_canvas()
 
 grass = load_image("grass.png")
 character = load_image("character.png")
 
-x =0
-while(x<800):
+x = 0
+y = 90
+init_y = 90
+
+character.draw_now(0,90)
+
+move_rect = False
+
+up_x = 700
+up_y = 550
+offset =0
+
+r = 5
+
+while(True):
     clear_canvas_now()
     grass.draw_now(400,30)
-    character.draw_now(x,90)
-    x = x+2
-    delay(0.01)
+    character.draw_now(x,y)
+    if ( move_rect):
+        if ( x < up_x and y == init_y):
+            x = x+2
+        elif (x >= up_x and y < up_y):
+            y= y+2
+        elif ( y>= up_y and x >= 0):
+            x = x-2
+        elif ( y >= init_y ):
+            y= y-2
+            move_rect = False
+    else:
+        # Move_circle
+        start_pos = 400
+        if ( x < start_pos):
+           x = x+2
+        else:    
+            offset = offset +10
+            theta_x  =r * math.cos( offset/360 * math.pi)
+            theta_y = r * math.sin(offset/360 * math.pi)
+            x= x + theta_x
+            y= y + theta_y
+
+    #delay(0.01)
 
 close_canvas()
