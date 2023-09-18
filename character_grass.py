17f0b64031d6bc42a2190469e3133bcd82a1a48f
jjuncode <jjun.code@gmail.com>
2023-09-18 13:04:11 +0900

    사각이동 Done

--- character_grass.py
+++ character_grass.py
@@ -8,46 +8,50 @@ character = load_image("character.png")
 
 x = 0
 y = 90
-init_y = 90
 
-character.draw_now(0,90)
+dir = 'right'
+offset =0 
 
 move_rect = True
 
-up_x = 700
-up_y = 550
-offset =0
-
-r = 10
+character.draw_now(0,90)
 
 while(True):
     clear_canvas_now()
     grass.draw_now(400,30)
     character.draw_now(x,y)
     if ( move_rect):
-        if ( x < up_x and y == init_y):
-            x = x+2
-        elif (x >= up_x and y < up_y):
-            y= y+2
-        elif ( y>= up_y and x >= 0):
-            x = x-2
-        elif ( y >= init_y ):
-            y= y-2
-            move_rect = False
+       if (dir == 'right' ):
+           x = x+2
+           if ( x >= 700 ):
+              dir = 'up'
+
+       elif (dir == 'up'):
+          y= y+2
+          if ( y >= 500 ):
+             dir = 'left'
+       elif (dir == 'left'):
+          x= x-2
+          if ( x <= 100):
+             dir = 'down'
+       elif ( dir == 'down'):
+          y= y-2
+          if ( y <=90 ):
+             dir = 'right'
+             move_rect = False
+
     else:
         # Move_circle
-        start_pos = 400
+        start_pos = 200
         if ( x < start_pos):
            x = x+2
-        else:    
-            offset = offset +10
-            theta_x  =r * math.cos( offset/360 * math.pi)
-            theta_y = r * math.sin(offset/360 * math.pi)
+        else:   
+            offset = offset +1
+            theta_x  =math.cos( (270+offset)/360*math.pi)
+            theta_Y = math.sin( (270+offset)/360*math.pi)
             x= x + theta_x
-            y= y + theta_y
-            if ( y== init_y ):
-                move_rect = True
+            y= y + theta_Y
 
-    delay(0.01)
+    #delay(0.01)
 
 close_canvas()
